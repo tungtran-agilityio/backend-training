71b4619ce20e54a8426ba8f132fa66c19853c907
Tung Tran <tung.tran@asnet.com.vn>
2025-07-23 16:00:23 +0700

    feat: enhance AuthController with detailed API response documentation
    - Added Swagger decorators to the login, refresh, and logout endpoints for improved API documentation.
    - Included descriptions for various response scenarios, enhancing clarity for API consumers.

--- social-app/src/auth/auth.controller.ts
+++ social-app/src/auth/auth.controller.ts
@@ -3,12 +3,23 @@ import { AuthService } from './auth.service';
 import { Request, Response } from 'express';
 import { RefreshTokenGuard } from 'src/common/guards/refresh-token.guard';
 import { LoginDto } from './dtos/login.dto';
+import {
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiBadRequestResponse({ description: 'Missing or invalid credentials' })
+  @ApiUnauthorizedResponse({ description: 'Missing or invalid token' })
+  @ApiInternalServerErrorResponse({
+    description: 'Unexpected server-side failure',
+  })
   async login(
     @Body() body: LoginDto,
     @Res({ passthrough: true }) res: Response,
@@ -26,6 +37,17 @@ export class AuthController {
 
   @UseGuards(RefreshTokenGuard)
   @Post('refresh')
+  @ApiUnauthorizedResponse({ description: 'Missing or invalid token' })
+  @ApiInternalServerErrorResponse({
+    description: 'Unexpected server-side failure',
+  })
+  @ApiOkResponse({
+    schema: {
+      example: {
+        accessToken: 'new-access-token',
+      },
+    },
+  })
   async refresh(@Req() req: Request) {
     // req.user is set by RefreshTokenStrategy
     const user = req.user as { userId: string; email: string };
@@ -38,6 +60,13 @@ export class AuthController {
   }
 
   @Post('logout')
+  @ApiOkResponse({ description: 'Logged out successfully' })
+  @ApiBadRequestResponse({
+    description: 'Refresh token missing or invalid format',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Unexpected server-side failure',
+  })
   logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     res.clearCookie('refresh_token', { path: '/auth/refresh' });
     return { message: 'Logged out successfully' };
