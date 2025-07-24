6abe9c1f30bbd32f90af23aa32a79295c0c34db9
Tung Tran <tung.tran@asnet.com.vn>
2025-07-24 10:28:25 +0700

    feat: add end-to-end tests for application health and authentication
    - Introduced comprehensive end-to-end tests for the application, covering health check and authentication endpoints.
    - Implemented database integration tests to verify user creation, retrieval, and handling of constraints.
    - Enhanced test setup with global application initialization and Prisma client management for reliable testing environment.

--- social-app/test/__test__/auth.e2e-spec.ts
+++ social-app/test/__test__/auth.e2e-spec.ts
@@ -2,8 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from '../src/app.module';
-import { PrismaClient } from '../generated/prisma';
+import { AppModule } from '../../src/app.module';
+import { PrismaClient } from '../../generated/prisma';
 
 describe('Auth (e2e)', () => {
   let app: INestApplication<App>;
